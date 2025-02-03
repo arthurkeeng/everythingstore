6a2d81e04da1b48a6b9a5d83d6402a813f02cf7c
arthurkeeng <keengarthurchima@gmail.com>
2025-02-03 15:53:25 +0100

    removing esling

--- eslint.config.js
+++ eslint.config.js
@@ -8,17 +8,15 @@ import pluginReact from "eslint-plugin-react";
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser },
-  settings: {
-    react:{version :  "detect"}
-  }
+ 
 },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 
-  {
-    ignores : [
-      ".nextjs" , "node_modules" , "coverage", 
-    ]
-  }
+  // {
+  //   ignores : [
+  //     ".nextjs" , "node_modules" , "coverage", 
+  //   ]
+  // }
 ];
